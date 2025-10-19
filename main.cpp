e4d16aa02370ab70ea68014ab76ccf5112010fe8
moonChen2 <bxwow@icloud.com>
2025-10-19 15:42:58 +0800

    Init glfw window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
-
+#include "GlfwGeneral.h"
 
 int main(){
-    std::cout<<"hello vulkan"<<std::endl;
+    if (!InitializeWindow({ 1280, 720 }))
+        return -1; //来个你讨厌的返回值
+    while (!glfwWindowShouldClose(pWindow)) {
+
+        /*渲染过程，待填充*/
+
+        glfwPollEvents();
+        TitleFps();
+    }
+    TerminateWindow();
 }
